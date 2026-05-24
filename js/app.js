af89443fc80cedbc222836ef797617f5b6d1be0d
BryanMar11 <ebmpepe22@gmail.com>
2026-05-24 16:00:17 -0500

    Agregando js

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const listaPersonajes = [
+    {
+        id: 1,
+        nombrePersonaje: "Peter Parker",
+        descripción: "El Spider-Man original de la Tierra-616. Científico brillante, fotógrafo y protector de Nueva York tras ser mordido por una araña radiactiva.",
+        habilidades: ["Sentido arácnido", "Fuerza sobrehumana", "Lanzatelarañas", "Adherencia a superficies"],
+        url_imagen: "https://images.unsplash.com/photo-1604200213928-ba3cf4fc8436?auto=format&fit=crop&w=600&q=80",
+        edad: 25,
+        altura: 1.78,
+        esVillano: false
+    },
+    {
+        id: 2,
+        nombrePersonaje: "Miles Morales",
+        descripción: "Joven de Brooklyn que asume el manto de Spider-Man. Además de las habilidades arácnidas clásicas, posee poderes bio-eléctricos únicos.",
+        habilidades: ["Rayo venom (Bio-electricidad)", "Camuflaje invisible", "Sentido arácnido", "Acrobacias de gran nivel"],
+        url_imagen: "https://images.unsplash.com/photo-1635805737707-575885ab0820?auto=format&fit=crop&w=600&q=80",
+        edad: 17,
+        altura: 1.73,
+        esVillano: false
+    }
+];
+
+console.log("Spider-Verse DB conectada exitosamente", listaPersonajes);
+
